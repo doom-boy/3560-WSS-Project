b5261605af9228e6db7b2cc49b0a1c311381e07f
elijah <eechan@cpp.edu>
2026-04-30 17:38:18 -0700

    set up traders event

---
+++ src/event/GenerousTrader.java
@@ -0,0 +1,5 @@
+package event;
+
+public class GenerousTrader extends Trader {
+    
+}
--- src/event/Trader.java
+++ src/event/Trader.java
@@ -1,7 +1,11 @@
 package event;
 
-public class Trader {
-    
+import player.Player;
+
+public abstract class Trader extends Event {
+    public Trader() {};
+
+    public void trigger(Player player) {}
 }
 
 // has to be abstract for the generous and greedy trader implementation

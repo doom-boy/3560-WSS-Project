1dfd745010653941653d5206d90b0c024331082f
elijah <eechan@cpp.edu>
2026-04-30 17:37:43 -0700

    Gold bonus event
    -modifiable w multiplier; determined by event generation

--- src/event/GoldBonus.java
+++ src/event/GoldBonus.java
@@ -1,6 +1,33 @@
 package event;
 
+import player.Player;
+
 public class GoldBonus extends Event {
+
+    private double multiplier;
+    private int baseAmount;
+
+    public GoldBonus(double multiplier, int baseAmount) {
+        super(false); // not repeating, 1 time event
+        this.multiplier = multiplier;
+        this.baseAmount = baseAmount;
+    }
+
+    @Override
+    public void trigger(Player player) {
+        if (!isAvailable()) return;
+        int reward = (int)(baseAmount * multiplier);
+        player.addGold(reward);
+        System.out.println("Gold Bonus! +" + reward + " gold");
+        setEncountered(true);
+    }
+
+
+    public double getMultiplier() {
+        return multiplier;
+    }
+
+
     
 }
 
